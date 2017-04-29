12ffbe0a10ca220621e414111e7f7566d4516ff1
Chris Bradley <chris.bradley.423@gmail.com>
2017-04-29 14:11:14 -0400

    fix readme.md

---
+++ src/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import VideoListItem from './video_list_item'
+
+
+
+const VideoList = (props) => {
+    return (
+        <ul className="col-md-4 offset-md-8 list-group">
+            {props.videos.map(video => {
+                return (
+                  <VideoListItem
+                      onVideoSelect={props.onVideoSelect}
+                      key={video.etag}
+                      video={video} />
+                );
+            })}
+        </ul>
+    );
+}
+
+export default VideoList;
